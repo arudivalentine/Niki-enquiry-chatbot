e5cfb3117958f992ee97c430972a3996c7d955ec
Arudi Valentine <40007505+arudivalentine@users.noreply.github.com>
2023-06-19 02:38:33 -0700

    Update app.py

--- template/app.py
+++ template/app.py
@@ -1,40 +1,39 @@
+import requests
 from flask import Flask, render_template, request, jsonify
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('chat.html')
 
-@app.route("/get", methods=["GET", "POST"])
+@app.route("/get", methods=["POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
+    response = get_chat_response(msg)
+    return jsonify({'response': response})
 
-def get_Chat_response(text):
-    
-    # Let's chat for 5 lines
-for step in range(5):
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-    new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+def get_chat_response(text):
+    # Make API call to OpenAI for chat generation
+    headers = {
+        "Authorization": "Bearer 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",
+        "OpenAI-Organization": "org-cmk8cdV1jDX11T4pQnhbv4qi"
+    }
+    data = {
+        "inputs": {
+            "text": text
+        }
+    }
+    response = requests.post("https://api.openai.com/v1/models/your-model-id/completions", headers=headers, json=data)
+    response_json = response.json()
+    completion_text = response_json["choices"][0]["text"].strip()
 
-    # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+    return completion_text
 
-    # generated a response while limiting the total chat history to 1000 tokens, 
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-    # pretty print last ouput tokens from bot
-    return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run()
-o
